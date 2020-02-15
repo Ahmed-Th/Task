0452f5616d2303f8f383890eaa94816cdb309eca
Ahmed-Th <60985434+Ahmed-Th@users.noreply.github.com>
2020-02-15 17:46:38 +0200

    Add files via upload

---
+++ react/src/Components/History/History.js
@@ -0,0 +1,45 @@
+import React from 'react';
+import { makeStyles } from '@material-ui/core/styles';
+
+const useStyles = makeStyles(theme => ({
+    history:{
+        textAlign:'start',
+        padding:'1rem 2rem',
+        lineHeight:'1.7rem',
+        
+    },
+    history__heading:{
+        fontWeight:'bolder',
+        fontSize:'1.4rem',
+        color:'black',    
+    },
+    history__about:{
+        color:'black',
+        marginTop:'-1rem',   
+    },
+    history__text:{
+
+    },
+    history__text_span:{
+        color:'black',    
+    },
+
+}));
+
+const History = () =>{
+    const classes = useStyles();
+  
+    return(
+        <div className={classes.history}>
+            <h2 className={classes.history__heading}>HISTORY</h2>
+            <h5 className={classes.history__about}>This article is about the American actor. </h5>
+            <p className={classes.history__text}>
+            <span className={classes.history__text_span} >Thomas Cruis Mapother IV</span>Lorem ipsum dolor sit amet consectetur adipisicing elit. Ut necessitatibus ex quae autem a odio Ut necessitatibus ex quae autem a odio
+            Lorem ipsum dolor sit amet consectetur adipisicing elit.sit amet consectetur adipisicing elit.sit amet consectetur adipisicing elit.sit amet consectetur adipisicing elit.sit amet consectetur adipisicing elit.sit amet consectetur adipisicing elit.sit amet consectetur adipisicing elit. Ut necessitatibus ex quae autem a odio Ut necessitatibus ex quae autem a odio
+            </p>
+        </div>
+    );
+
+}
+
+export default History;
